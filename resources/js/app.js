44b92b877f3dd906846586f791a72f80925ea89e
Pierre Cordier <400notes@gmail.com>
2022-10-01 09:32:28 +0200

    work on vue files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,25 @@
-"use strict";
-var _a;
-exports.__esModule = true;
-require("../css/app.postcss");
-var vue_1 = require("vue");
-var inertia_vue3_1 = require("@inertiajs/inertia-vue3");
-var progress_1 = require("@inertiajs/progress");
-var inertia_helpers_1 = require("laravel-vite-plugin/inertia-helpers");
-var vue_m_1 = require("../../vendor/tightenco/ziggy/dist/vue.m");
-var appName = ((_a = window.document.getElementsByTagName('title')[0]) === null || _a === void 0 ? void 0 : _a.innerText) || 'Laravel';
-var pinia = createPinia();
-(0, inertia_vue3_1.createInertiaApp)({
-    title: function (title) { return "".concat(title, " - ").concat(appName); },
-    resolve: function (name) { return (0, inertia_helpers_1.resolvePageComponent)("./Pages/".concat(name, ".vue"), import.meta.glob('./Pages/**/*.vue')); },
-    setup: function (_a) {
-        var el = _a.el, app = _a.app, props = _a.props, plugin = _a.plugin;
-        return (0, vue_1.createApp)({ render: function () { return (0, vue_1.h)(app, props); } })
+import '../css/app.postcss';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const pinia = createPinia()
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(pinia)
-            .use(vue_m_1.ZiggyVue, Ziggy)
+            .use(ZiggyVue, Ziggy)
             .mount(el);
-    }
+    },
 });
-progress_1.InertiaProgress.init({ color: '#4B5563' });
+
+InertiaProgress.init({ color: '#4B5563' });
